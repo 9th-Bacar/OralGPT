34aa5d8b833a4562e87bef05df7f8846746583dc
9th-Bacar <932581735@qq.com>
2023-02-08 17:54:39 +0800

    add css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,229 @@
-import logo from './logo.svg';
-import Dictaphone from './ChatPage'
-import './App.css';
-
+import logo from "./gptLogo.png";
+import Dictaphone from "./ChatPage";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
 function App() {
+  const [recognition, setRecognition] = useState("browser");
+  const [goodToStart, setGoodToStart] = useState(false);
+  const [azureApiKey, setAzureApiKey] = useState("");
+  const [azureApiRegion, setAzureApiRegion] = useState("");
+  const [openAiApiKey, setOpenAiApiKey] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    setOpenAiApiKey(localStorage.getItem("openAiApiKey"));
+    setAzureApiKey(localStorage.getItem("azureApiKey"));
+    setAzureApiRegion(localStorage.getItem("azureApiRegion"));
+    setRecognition(localStorage.getItem("recognition"));
+    
+  }, []);
+
+  useEffect(() => {
+    CheckApiConfig()
+  }, [openAiApiKey, azureApiKey, azureApiRegion, recognition]);
+
+
+  const Headers = () => {
+    return (
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">Oral GPT</a>
+        </div>
+
+        <div className="dropdown dropdown-end">
+          <button tabIndex={0} className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+            </svg>
+          </button>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <a>Language</a>
+            </li>
+            <li>
+              <a>Speaker Option</a>
+            </li>
+            <li
+              onClick={() => {
+                setModalOpen(true);
+              }}>
+              <a>API Option</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  const StartBottom = (
+    <label
+      htmlFor="my-modal"
+      className="btn"
+      onClick={() => {
+        setModalOpen(true);
+      }}>
+      Start
+    </label>
+  );
+
+  const ApiKeyInput = (
+    <div className="form-control w-full max-w-xs">
+      <label className="label">
+        <span className="label-text">OpenAI Api Key:</span>
+      </label>
+      <input
+        type="text"
+        id="openaiAPiKey"
+        key="openaiAPiKey"
+        value={openAiApiKey}
+        placeholder="Type here"
+        className="input input-bordered w-full max-w-xs"
+        onChange={(e) => {
+          setOpenAiApiKey(e.target.value);
+        }}
+      />
+    </div>
+  );
+
+  const AzureApiInput = (
+    <div className="form-control w-full max-w-xs">
+      <label className="label">
+        <span className="label-text">Azure Api Key:</span>
+      </label>
+      <input
+        type="text"
+        placeholder="Type here"
+        id="azureApiKey"
+        className="input input-bordered w-full max-w-xs"
+        value={azureApiKey}
+        onChange={(e) => {
+          setAzureApiKey(e.target.value);
+        }}
+      />
+      <label className="label">
+        <span className="label-text">Azure Api Region:</span>
+      </label>
+      <input
+        type="text"
+        id="azureApiRegion"
+        placeholder="Type here"
+        className="input input-bordered w-full max-w-xs"
+        value={azureApiRegion}
+        onChange={(e) => {
+          setAzureApiRegion(e.target.value);
+        }}
+      />
+    </div>
+  );
+
+  const RecognitionOption = (
+    <div className="">
+      <label className="label">
+        <span className="label-text">Speech Recognition:</span>
+      </label>
+      <select
+        className="select select-bordered w-full max-w-xs"
+        value={recognition}
+        onChange={(e) => {
+          setRecognition(e.target.value);
+          console.log(e.target.value);
+        }}>
+        <option value="browser">Browser</option>
+        <option value="azure">Azure</option>
+      </select>
+      {recognition == "azure" && AzureApiInput}
+    </div>
+  );
+
+  const StartModal = (
+    <div>
+      <div
+        className={modalOpen ? "modal modal-open" : "modal"}
+        style={{ visibility: "visible" }}>
+        <div className="modal-box">
+          <label
+            htmlFor="my-modal-3"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+            onClick={() => {
+              setModalOpen(false);
+            }}>
+            ✕
+          </label>
+          <h3 className="font-bold text-lg">
+            Before you start, please enter your API key.
+          </h3>
+          {ApiKeyInput}
+          {RecognitionOption}
+          <div className="modal-action">
+            <label
+              htmlFor="my-modal"
+              className="btn"
+              onClick={() => {
+                saveApiConfig();
+              }}>
+              Save
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  const saveApiConfig = () => {
+    try {
+      localStorage.setItem("openAiApiKey", openAiApiKey);
+      localStorage.setItem("recognition", recognition);
+      localStorage.setItem("azureApiKey", azureApiKey);
+      localStorage.setItem("azureApiRegion", azureApiRegion);
+      toast.success("Save Successfully");
+      CheckApiConfig();
+    } catch (error) {}
+  };
+
+  const CheckApiConfig = () => {
+    if (recognition == "browser") {
+      openAiApiKey ? setGoodToStart(true) : setGoodToStart(false);
+    } else {
+      openAiApiKey && azureApiKey && azureApiRegion
+        ? setGoodToStart(true)
+        : setGoodToStart(false);
+    }
+  };
+
+  const StartContainer = (
+    <body className="flex items-center justify-center h-[200px] bg-gray-400">
+      
+      {StartBottom}
+    </body>
+  );
+
+  const ChatContainer = (
+    <body className=" bg-gray-400">
+      <Dictaphone openAiApiKey={{openAiApiKey}} azureApiKey={{azureApiKey}} azureApiRegion={{azureApiRegion}} recognitionType={{recognition}} />
+    </body>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="h-screen bg-gray-200 ">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Dictaphone />
-      </header>
-
+      </header> */}
+      <Headers />
+      <span className="">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</span>
+      {!goodToStart ? StartContainer : ChatContainer}
+      {StartModal}
     </div>
   );
 }
--- src/ChatPage.js
+++ src/ChatPage.js
@@ -1,104 +1,172 @@
-import React, { useEffect, useState } from 'react';
-import createSpeechServicesPonyfill from 'web-speech-cognitive-services';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import { Configuration, OpenAIApi } from 'openai'
-import { useSpeechSynthesis } from 'react-speech-kit';
+import React, { useEffect, useState } from "react";
+import createSpeechServicesPonyfill from "web-speech-cognitive-services";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { Configuration, OpenAIApi } from "openai";
+import { useSpeechSynthesis } from "react-speech-kit";
+import { toast } from "react-hot-toast";
 // import sdk from 'microsoft-cognitiveservices-speech-sdk'
 // import Buffer from 'buffer'
 // import PassThrough from 'stream'
 // import fs from 'fs'
 
-const SUBSCRIPTION_KEY = '6a29b899538c49fba2dbaf5a6f65ae27';
-const REGION = 'southeastasia';
+// const SUBSCRIPTION_KEY = "6a29b899538c49fba2dbaf5a6f65ae27";
+// const REGION = "southeastasia";
 
-const { SpeechRecognition: AzureSpeechRecognition } = createSpeechServicesPonyfill({
+
+
+
+
+const Dictaphone1 = (props) => {
+  const recognitionType = props.recognitionType;
+  const azureApiKey = props.azureApiKey;
+  const azureApiRegion = props.azureApiRegion;
+  const openAiApiKey = props.openAiApiKey;
+  const configuration = new Configuration({
+    // apiKey: "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",
+    apiKey: openAiApiKey,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  if (!openAiApiKey){
+    toast.error("OpenAI Api Key is required");
+  }else{
+  }
+
+  const { SpeechRecognition: AzureSpeechRecognition } =
+  createSpeechServicesPonyfill({
     credentials: {
-        region: REGION,
-        subscriptionKey: SUBSCRIPTION_KEY,
-    }
-});
+      region: azureApiRegion,
+      subscriptionKey: azureApiKey,
+    },
+  });
 
-const configuration = new Configuration({
-    apiKey: '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',
-});
-const openai = new OpenAIApi(configuration);
+  recognitionType=='azure'&&SpeechRecognition.applyPolyfill(AzureSpeechRecognition);
 
-SpeechRecognition.applyPolyfill(AzureSpeechRecognition);
-const Dictaphone1 = () => {
-    const [message, setMessage] = useState('');
-    const { speak } = useSpeechSynthesis();
-    const commands = [
-        {
-            command: 'reset',
-            callback: () => resetTranscript()
-        },
-        {
-            command: 'shut up',
-            callback: () => setMessage('I wasn\'t talking.')
-        },
-        {
-            command: 'Hello',
-            callback: () => setMessage('Hi there!')
-        },
-    ]
-    const {
-        transcript,
-        interimTranscript,
-        finalTranscript,
-        resetTranscript,
-        listening,
-    } = useSpeechRecognition({ commands });
+  const [message, setMessage] = useState("");
+  const { speak } = useSpeechSynthesis();
+  const [reply, setReply] = useState("");
+  const [promptTemp, setPromptTemp] = useState(
+    "The following is a conversation with an AI assistant.The assistant is helpful, creative, clever, and very friendly.\n\n"
+  );
+  const commands = [
+    {
+      command: "reset",
+      callback: () => resetTranscript(),
+    },
+    // {
+    //   command: "shut up",
+    //   callback: () => setMessage("I wasn't talking."),
+    // },
+    // {
+    //   command: "Hello",
+    //   callback: () => setMessage("Hi there!"),
+    // },
+  ];
+  const {
+    transcript,
+    interimTranscript,
+    finalTranscript,
+    resetTranscript,
+    listening,
+  } = useSpeechRecognition({ commands });
 
-    const askGPT = async () => {
-        const completion = await openai.createCompletion({
-            model: "text-davinci-002",
-            prompt: finalTranscript,
-        });
-        console.log(completion.data);
-        speak({ text: completion.data.choices[0].text })
-        resetTranscript()
-    }
-    useEffect(() => {
-        if (finalTranscript !== '') {
-            console.log('Got final result:', finalTranscript);
-            console.log(interimTranscript);
-            // askGPT()
-        }
-    }, [interimTranscript, finalTranscript]);
-    // if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-    //     return null;
-    // }
-    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-        console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
-    }
-    const listenContinuously = () => {
-        SpeechRecognition.startListening({
-            continuous: true,
-            language: 'en-GB',
-        });
-    };
-    return (
-        <div>
-            <div>
-                <span>
-                    listening:
-                    {' '}
-                    {listening ? 'on' : 'off'}
-                </span>
-                <div>
-                    <button type="button" onClick={resetTranscript}>Reset</button>
-                    <button type="button" onClick={listenContinuously}>Listen</button>
-                    <button type="button" onClick={SpeechRecognition.stopListening}>Stop</button>
-                    <button type="button" onClick={askGPT}>send</button>
-                </div>
-            </div>
-            <div>
-                {message}
-            </div>
-            <div>
-                <span>{transcript}</span>
-            </div>
-        </div>
+  const resetAll = () => {
+    resetTranscript();
+    setPromptTemp(
+      "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n"
     );
+  };
+
+  const askGPT = async () => {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: promptTemp + `Human: ${finalTranscript}`,
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: [" Human:", " AI:"],
+    });
+
+    console.log(completion.data);
+    setReply(completion.data.choices[0].text);
+    setPromptTemp(
+      promptTemp +
+        `Human: ${finalTranscript}\n` +
+        `${completion.data.choices[0].text}\n`
+    );
+    speak({
+      text: completion.data.choices[0].text
+        .replace("AI assistant:", "")
+        .replace("AI", "")
+        .replace("AI Assistant:", ""),
+    });
+    resetTranscript();
+  };
+  useEffect(() => {
+    if (finalTranscript !== "") {
+      console.log("Got final result:", finalTranscript);
+      console.log(interimTranscript);
+      //   askGPT()
+    }
+  }, [interimTranscript, finalTranscript]);
+  //   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+  //       return null;
+  //   }
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    console.log(
+      "Your browser does not support speech recognition software! Try Chrome desktop, maybe?"
+    );
+  }
+  const listenContinuously = () => {
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: "zh-CN",
+    });
+  };
+
+  const ChatDisplay = () => (
+    <div>
+      {promptTemp.split("\n").map((line, index) => (
+        <p>
+          {/* {React.createElement('br')} */}
+          {line}
+        </p>
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="bg-white w-screen h-[500px]">
+      <div>
+
+        <span>listening: {listening ? "on" : "off"}</span>
+        <div>
+          <button type="button" onClick={resetAll}>
+            Reset
+          </button>
+          <button type="button" onClick={listenContinuously}>
+            Listen
+          </button>
+          <button type="button" onClick={SpeechRecognition.stopListening}>
+            Stop
+          </button>
+          <button type="button" onClick={askGPT}>
+            send
+          </button>
+        </div>
+      </div>
+      {/* <div>{message}</div>
+       */}
+
+      {ChatDisplay()}
+      <div>
+        <span>{transcript}</span>
+      </div>
+    </div>
+  );
 };
 export default Dictaphone1;
